5c4ebd0f03b60116d2d75cbf10f6dbac037b86f1
NightOwl Studios <nightowl@MacMini.local>
2013-03-06 15:15:27 +0000

    primeiro commit projecto xcode

---
+++ TJS/TJS/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TJS
+//
+//  Created by NightOwl Studios on 06/03/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TJS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TJS
+//
+//  Created by NightOwl Studios on 06/03/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
