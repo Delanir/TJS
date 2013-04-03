efbba30a49d1bd9e8a55089fadfe11a40c09d88b
João Machado <joaoprmachado@gmail.com>
2013-04-03 11:43:14 +0100

    Read TODO

---
+++ TechDemo - Physics/TechDemo - Physics/EnemyFactory.h
@@ -0,0 +1,13 @@
+//
+//  EnemyFactory.h
+//  TechDemo - Physics
+//
+//  Created by jp on 03/04/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EnemyFactory : NSObject
+
+@end
---
+++ TechDemo - Physics/TechDemo - Physics/EnemyFactory.m
@@ -0,0 +1,13 @@
+//
+//  EnemyFactory.m
+//  TechDemo - Physics
+//
+//  Created by jp on 03/04/13.
+//
+//
+
+#import "EnemyFactory.h"
+
+@implementation EnemyFactory
+
+@end
