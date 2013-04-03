b360197077636cd6105d9e9fa48a5eb61bb343b3
Delanir <danielafilipa@gmail.com>
2013-04-03 02:50:37 +0100

    v2 sprite test

---
+++ TechDemo - Sprite Performance v2/Cocos2DSimpleGame/CCMenu_CCMenu_Slider.h
@@ -0,0 +1,16 @@
+//
+//  CCMenu_CCMenu_Slider.h
+//  TechDemo - Sprites
+//
+//  Created by MiniclipMacBook on 4/2/13.
+//  Copyright (c) 2013 TJS. All rights reserved.
+//
+
+#import "CCMenu.h"
+
+@interface CCMenu (CCMenu_CCMenu_Slider)
+
+-(void) ccTouchMoved:(UITouch *)touch withEvent:(UIEvent *)event;
+
+
+@end
