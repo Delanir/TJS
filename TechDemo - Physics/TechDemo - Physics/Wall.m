c57ef1ee8abec30b15134aa37f9ec9c2f9b6f639
João Machado <joaoprmachado@gmail.com>
2013-04-12 15:08:37 +0100

    - Mt lento a mudar de castelo. Talvez seja melhor estar na sprite sheet     - Resolvido carregando todas as sprites no init     - Fica hardcoded, mas MUITO mais eficiente - Continua a poder ser possível meter na spritesheet

--- TechDemo - Physics/TechDemo - Physics/Wall.m
+++ TechDemo - Physics/TechDemo - Physics/Wall.m
@@ -29,20 +29,41 @@ static Wall * _major = nil;
         
         CGSize winSize = [[CCDirector sharedDirector] winSize];
         
-        KKPixelMaskSprite * castletop = [KKPixelMaskSprite spriteWithFile:@"castle100-top.png" alphaThreshold:0.5f];
-        KKPixelMaskSprite * castlebottom = [KKPixelMaskSprite spriteWithFile:@"castle100-bottom.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castletop100 = [KKPixelMaskSprite spriteWithFile:@"castle100-top.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castlebottom100 = [KKPixelMaskSprite spriteWithFile:@"castle100-bottom.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castletop75 = [KKPixelMaskSprite spriteWithFile:@"castle75-top.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castlebottom75 = [KKPixelMaskSprite spriteWithFile:@"castle75-bottom.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castletop50 = [KKPixelMaskSprite spriteWithFile:@"castle50-top.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castlebottom50 = [KKPixelMaskSprite spriteWithFile:@"castle50-bottom.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castletop25 = [KKPixelMaskSprite spriteWithFile:@"castle25-top.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castlebottom25 = [KKPixelMaskSprite spriteWithFile:@"castle25-bottom.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castletop0 = [KKPixelMaskSprite spriteWithFile:@"castle0-top.png" alphaThreshold:0.5f];
+        KKPixelMaskSprite * castlebottom0 = [KKPixelMaskSprite spriteWithFile:@"castle0-bottom.png" alphaThreshold:0.5f];
         
-        [sprites addObject:castletop];
-        [sprites addObject:castlebottom];
+        [sprites addObject:castletop100];
+        [sprites addObject:castlebottom100];
+        [sprites addObject:castletop75];
+        [sprites addObject:castlebottom75];
+        [sprites addObject:castletop50];
+        [sprites addObject:castlebottom50];
+        [sprites addObject:castletop25];
+        [sprites addObject:castlebottom25];
+        [sprites addObject:castletop0];
+        [sprites addObject:castlebottom0];
         
-        [castletop setPosition:ccp(winSize.width/2, winSize.height/2)];
-        [castlebottom setPosition:ccp(winSize.width/2, winSize.height/2)];
+        CGPoint center = ccp(winSize.width/2, winSize.height/2);
         
-        [parentNode addChild:castletop z:1500];
-        [parentNode addChild:castlebottom z:10];
+        for (CCSprite * spr in sprites)
+        {
+            [spr setPosition:center];
+            [spr setTag:5];
+        }
         
-        [[CollisionManager shared] addToWalls:castletop];
-        [[CollisionManager shared] addToWalls:castlebottom];
+        [parentNode addChild:castletop100 z:1500];
+        [parentNode addChild:castlebottom100 z:10];
+        
+        [[CollisionManager shared] addToWalls:castletop100];
+        [[CollisionManager shared] addToWalls:castlebottom100];
         
         
         [self schedule:@selector(update:)];
@@ -59,22 +80,22 @@ static Wall * _major = nil;
     {
         if (health < 75 && health >= 50 && status == mint)
         {
-            [self changeTopSprite:@"castle75-top.png" bottomSprite:@"castle75-bottom.png"];
+            [self changeTopSprite:sprites[2] bottomSprite:sprites[3]];
             status = scratched;
         }
         else if (health < 50 && health >= 25 && status == scratched)
         {
-            [self changeTopSprite:@"castle50-top.png" bottomSprite:@"castle50-bottom.png"];
+            [self changeTopSprite:sprites[4] bottomSprite:sprites[5]];
             status = damaged;
         }
         else if (health < 25 && health > 0 && status == damaged)
         {
-            [self changeTopSprite:@"castle25-top.png" bottomSprite:@"castle25-bottom.png"];
+            [self changeTopSprite:sprites[6] bottomSprite:sprites[7]];
             status = wrecked;
         }
         else if (health <= 0 && status == wrecked)
         {
-            [self changeTopSprite:@"castle0-top.png" bottomSprite:@"castle0-bottom.png"];
+            [self changeTopSprite:sprites[8] bottomSprite:sprites[9]];
             status = totaled;
         }
         lastHealth = health;
@@ -82,36 +103,34 @@ static Wall * _major = nil;
         
 }
 
-- (void) changeTopSprite: (NSString*) topSprite bottomSprite: (NSString*) bottomSprite
+- (void) changeTopSprite: (CCSprite*) topSprite bottomSprite: (CCSprite*) bottomSprite
 {
     
-    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    //CGSize winSize = [[CCDirector sharedDirector] winSize];
     [parentNode removeChild:sprites[0] cleanup:YES];
     [parentNode removeChild:sprites[1] cleanup:YES];
-    [sprites removeObject:sprites[1]];                      // reverse order is required!
-    [sprites removeObject:sprites[0]];
+    //[sprites removeObject:sprites[1]];                      // reverse order is required!
+    //[sprites removeObject:sprites[0]];
     
-    KKPixelMaskSprite * castletop = [KKPixelMaskSprite spriteWithFile:topSprite alphaThreshold:0.5f];
-    KKPixelMaskSprite * castlebottom = [KKPixelMaskSprite spriteWithFile:bottomSprite alphaThreshold:0.5f];
+    //KKPixelMaskSprite * castletop = [KKPixelMaskSprite spriteWithFile:topSprite alphaThreshold:0.5f];
+    //KKPixelMaskSprite * castlebottom = [KKPixelMaskSprite spriteWithFile:bottomSprite alphaThreshold:0.5f];
     
-    [sprites addObject:castletop];
-    [sprites addObject:castlebottom];
+    //[sprites addObject:castletop];
+    //[sprites addObject:castlebottom];
     
-    [castletop setPosition:ccp(winSize.width/2, winSize.height/2)];
-    [castlebottom setPosition:ccp(winSize.width/2, winSize.height/2)];
+    //[castletop setPosition:ccp(winSize.width/2, winSize.height/2)];
+    //[castlebottom setPosition:ccp(winSize.width/2, winSize.height/2)];
     
-    [parentNode addChild:castletop z:1500];
-    [parentNode addChild:castlebottom z:10];
+    [parentNode addChild:topSprite z:1500];
+    [parentNode addChild:bottomSprite z:10];
     
     
 }
 
 - (void) dealloc
 {
-    [parentNode removeChild:sprites[0] cleanup:YES];
-    [parentNode removeChild:sprites[1] cleanup:YES];
-    [sprites removeObject:sprites[1]];                      // reverse order is required!
-    [sprites removeObject:sprites[0]];
+    [parentNode removeChildByTag:5 cleanup:YES];
+    [sprites removeAllObjects];
     [super dealloc];
 }
 
