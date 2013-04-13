300329fdbb0e1dba9000df3b226b202cd1aed77c
João Machado <joaoprmachado@gmail.com>
2013-04-13 23:23:53 +0100

    - Criados estimulos e gerador de estimulos - Criados placeholders para estimulos restantes

--- TechDemo - Physics/TechDemo - Physics/Arrow.m
+++ TechDemo - Physics/TechDemo - Physics/Arrow.m
@@ -7,7 +7,100 @@
 //
 
 #import "Arrow.h"
+#import "Stimulus.h"
 
 @implementation Arrow
+
+
+
+- (id) initWithDestination: (CGPoint) location andStimulusPackage: (CCArray *) stimulusPackage
+{
+    CGSize winSize = [[CCDirector sharedDirector] winSize];
+    if( self = [super initWithSprite:@"Projectile.png"])
+    {
+        
+        CGSize spriteSize = [self spriteSize];
+        
+        // @TODO init with yuri information
+        sprite.position = ccp(190, winSize.height/2 + 28);
+    
+        // Determine offset of location to projectile
+        int offX = location.x - sprite.position.x;
+        int offY = location.y - sprite.position.y;
+        
+        // Bail out if we are shooting down or backwards
+        if (offX <= 0)
+            offX = 1;
+        
+        // Determine where we wish to shoot the projectile to
+        int realX = winSize.width + (spriteSize.width/2);
+        float ratio = (float) offY / (float) offX;
+        int realY = (realX * ratio) + sprite.position.y;
+        [self setDestination:ccp(realX, realY)];
+        
+        // Determine the length of how far we're shooting
+        int offRealX = realX - sprite.position.x;
+        int offRealY = realY - sprite.position.y;
+        float length = sqrtf((offRealX*offRealX)+(offRealY*offRealY));
+        float velocity = 480/1; // 480pixels/1sec
+        [self setTimeToLive:length/velocity];
+        
+        // Rotate the arrow in the correct direction
+        float angle = CC_RADIANS_TO_DEGREES(atanf((float)offRealY / (float)offRealX));
+        [sprite setRotation:(-1 * angle)];
+        
+        // Move projectile to actual endpoint
+        [sprite runAction:[CCSequence actions:
+                           [CCMoveTo actionWithDuration:[self timeToLive] position:[self destination]],
+                           [CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)],
+                           nil]];
+        
+        [self handleStimulus:stimulusPackage];
+    }
+    return self;
+}
+
+
+- (void) handleStimulus:(CCArray *) stimulusPackage;
+{
+    CCParticleSystem * ps;
+    for(Stimulus * stimulus in stimulusPackage)
+    {
+        switch ([stimulus type])
+        {
+            case slow:
+                
+                ps = [[CCParticleMeteor node] retain];
+                [ps setTag:7];
+                [self addChild:ps];
+                
+                //  ps.startSize = 15;
+                ps.gravity = CGPointZero;
+                ps.life = 0;
+                //  ps.totalParticles = 25;
+                ps.position = sprite.position;
+                
+                
+                // Move Particle system to actual endpoint
+                [ps runAction:[CCSequence actions:
+                               [CCMoveTo actionWithDuration:[self timeToLive] position:[self destination]],
+                               //[CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)],
+                               nil]];
+                break;
+            case dot:
+                // create particle para isto
+                NSLog(@"DAMAGE OVER TIME");
+                break;
+            case damage:
+                NSLog(@"JUST DAMAGE!!");
+                break;
+            case pushBack:
+                NSLog(@"PUSH IT");
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 @end
--- TechDemo - Physics/TechDemo - Physics/Projectile.m
+++ TechDemo - Physics/TechDemo - Physics/Projectile.m
@@ -11,84 +11,29 @@
 
 @implementation Projectile
 
+@synthesize destination, timeToLive;
+
 -(void)spriteMoveFinished:(id)sender {
-    [self removeChild:ps cleanup:YES];
-// CCLOG(@"Morreu PS");
-// CCLOG(@"Morreu Sprite");
-    [self removeChild:sprite cleanup:YES];
+    [self destroy];
     [[CollisionManager shared] removeFromProjectiles:self];
 }
 
-- (id) initWithSprite:(NSString *)spriteFile andLocation:(CGPoint)location andWindowSize:(CGSize)winSize
+- (id) initWithSprite:(NSString *)spriteFile
 {
     if( (self=[super init])) {
-      
+        
         [self setSpriteWithSpriteFrameName:spriteFile];
         [self addChild:sprite];
-      
-        ps = [[CCParticleMeteor node] retain];
-        [self addChild:ps];
-      
-      //  ps.startSize = 15;
-      ps.gravity = CGPointZero;
-      ps.life = 0;
-      //  ps.totalParticles = 25;
-      sprite.position = ccp(190, winSize.height/2 + 28);                 // @TODO init with yuri information
-      ps.position = sprite.position;
-      
-        CGSize spriteSize = [self spriteSize];
-        CCLOG(@">>> X: %f  Y: %f\n", sprite.position.x, sprite.position.y);
-        // Determine offset of location to projectile
-        int offX = location.x - sprite.position.x;
-        int offY = location.y - sprite.position.y;
         
-        // Bail out if we are shooting down or backwards
-        if (offX <= 0)
-            offX = 1;
-        
-        // Determine where we wish to shoot the projectile to
-        int realX = winSize.width + (spriteSize.width/2);
-        float ratio = (float) offY / (float) offX;
-        int realY = (realX * ratio) + sprite.position.y;
-        CGPoint realDest = ccp(realX, realY);
-        
-        // Determine the length of how far we're shooting
-        int offRealX = realX - sprite.position.x;
-        int offRealY = realY - sprite.position.y;
-        float length = sqrtf((offRealX*offRealX)+(offRealY*offRealY));
-        float velocity = 480/1; // 480pixels/1sec
-        float realMoveDuration = length/velocity;
-        
-        // Rotate the arrow in the correct direction
-        float angle = CC_RADIANS_TO_DEGREES(atanf((float)offRealY / (float)offRealX));
-        [sprite setRotation:(-1 * angle)];
-        
-   
-      
-      // Move Particle system to actual endpoint
-      [ps runAction:[CCSequence actions:
-                         [CCMoveTo actionWithDuration:realMoveDuration position:realDest],
-                         //[CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)],
-                         nil]];
-      
-      // Move projectile to actual endpoint
-      [sprite runAction:[CCSequence actions:
-                         [CCMoveTo actionWithDuration:realMoveDuration position:realDest],
-                         [CCCallFuncN actionWithTarget:self selector:@selector(spriteMoveFinished:)],
-                         nil]];
-      
     }
-    
     return self;
-    
 }
 
-- (void) destroyParticleSystem
+- (void) destroy
 {
-  [self removeChild:ps cleanup:YES];
+    [self removeAllChildrenWithCleanup:YES];
 }
 
-//-(void)addStimulous
 
 
 
