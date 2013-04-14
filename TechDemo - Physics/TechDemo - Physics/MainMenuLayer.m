c1b69fa8e9df4b2680f2106a84b65ba3e54bb250
Delanir <danielafilipa@gmail.com>
2013-04-14 01:32:27 +0100

    Menu Inicial mais 4 butoes

--- TechDemo - Physics/TechDemo - Physics/MainMenuLayer.m
+++ TechDemo - Physics/TechDemo - Physics/MainMenuLayer.m
@@ -15,30 +15,53 @@
 #import "Config.h"
 // Sound interface
 #import "SimpleAudioEngine.h"
+// Needed to obtain the Navigation Controller
+#import "AppDelegate.h"
 
 
 @implementation MainMenuLayer
 
 
 
+- (void) pressedSettings:(id)sender
+{
+    CCLOG(@"Setting Menu");
+}
+
+- (void) pressedSkillTree:(id)sender
+{
+    CCLOG(@"Skilltree Menu");
+}
+
+- (void) pressedAchievments:(id)sender
+{
+    GKAchievementViewController *achivementViewController = [[GKAchievementViewController alloc] init];
+    achivementViewController.achievementDelegate = self;
+    
+    AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+    
+    [[app navController] presentModalViewController:achivementViewController animated:YES];
+    
+    [achivementViewController release];
+}
+
+- (void) pressedLeaderboard:(id)sender
+{
+    GKLeaderboardViewController *leaderboardViewController = [[GKLeaderboardViewController alloc] init];
+    leaderboardViewController.leaderboardDelegate = self;
+    
+    AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+    
+    [[app navController] presentModalViewController:leaderboardViewController animated:YES];
+    
+    [leaderboardViewController release];
+}
 
 - (void) pressedPlay:(id)sender
 {
     // ask director for the window size
 	CGSize size = [[CCDirector sharedDirector] winSize];
     
-//	CCSprite *background;
-//	
-//	if( UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone ) {
-//		background = [CCSprite spriteWithFile:@"Default.png"];
-//		background.rotation = 90;
-//	} else {
-//		background = [CCSprite spriteWithFile:@"Default-Landscape~ipad.png"];
-//	}
-//	background.position = ccp(size.width/2, size.height/2);
-//    
-//	// add the label as a child to this Layer
-//	[self addChild: background];
 	
 	// In one second transition to the new scene
 	[self scheduleOnce:@selector(makeTransition:) delay:1];
@@ -61,6 +84,20 @@
 -(void) makeTransition:(ccTime)dt
 {
 	[[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0 scene:[LevelLayer scene] withColor:ccWHITE]];
+}
+
+#pragma mark GameKit delegate
+
+-(void) achievementViewControllerDidFinish:(GKAchievementViewController *)viewController
+{
+	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+	[[app navController] dismissModalViewControllerAnimated:YES];
+}
+
+-(void) leaderboardViewControllerDidFinish:(GKLeaderboardViewController *)viewController
+{
+	AppController *app = (AppController*) [[UIApplication sharedApplication] delegate];
+	[[app navController] dismissModalViewControllerAnimated:YES];
 }
 
 @end
