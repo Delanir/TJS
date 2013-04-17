cb9044ab21f7cbe856fefc77b6485c7784556c09
Delanir <danielafilipa@gmail.com>
2013-04-17 01:27:40 +0100

    Skilltree xominominau

--- TechDemo - Physics/TechDemo - Physics/SkillTreeLayer.m
+++ TechDemo - Physics/TechDemo - Physics/SkillTreeLayer.m
@@ -15,6 +15,15 @@
 -(void)onEnter
 {
     [super onEnter];
+    
+    [_iceMainBranch setVisible:NO];
+    [_iceElement2 setVisible:NO];
+    [_iceElement1 setVisible:NO];
+    [_iceElement3 setVisible:NO];
+    [_iceBranch3 setVisible:NO];
+    [_iceBranch2 setVisible:NO];
+    [_iceBranch1 setVisible:NO];
+    
     [_cityMainBranch setVisible:NO];
 //    [_cityMainBranch setZOrder:15];
     [_cityElement2 setVisible:NO];
@@ -29,6 +38,22 @@
 //    [_cityBranch2 setZOrder:13];
     [_cityBranch1 setVisible:NO];
 //    [_cityBranch1 setZOrder:14];
+    
+    [_fireMainBranch setVisible:NO];
+    [_fireElement2 setVisible:NO];
+    [_fireElement1 setVisible:NO];
+    [_fireElement3 setVisible:NO];
+    [_fireBranch3 setVisible:NO];
+    [_fireBranch2 setVisible:NO];
+    [_fireBranch1 setVisible:NO];
+    
+    [_marksmanMainBranch setVisible:NO];
+    [_marksmanElement2 setVisible:NO];
+    [_marksmanElement1 setVisible:NO];
+    [_marksmanElement3 setVisible:NO];
+    [_marksmanBranch3 setVisible:NO];
+    [_marksmanBranch2 setVisible:NO];
+    [_marksmanBranch1 setVisible:NO];
 }
 
 - (void) pressedCitySymbol:(id)sender{
@@ -39,6 +64,36 @@
     [_cityBranch3 setVisible:YES];
     [_cityBranch2 setVisible:YES];
     [_cityBranch1 setVisible:YES];
+}
+
+- (void) pressedFireSymbol:(id)sender{
+    [_fireMainBranch setVisible:YES];
+    [_fireElement2 setVisible:YES];
+    [_fireElement1 setVisible:YES];
+    [_fireElement3 setVisible:YES];
+    [_fireBranch3 setVisible:YES];
+    [_fireBranch2 setVisible:YES];
+    [_fireBranch1 setVisible:YES];
+}
+
+- (void) pressedMarksmanSymbol:(id)sender{
+    [_marksmanMainBranch setVisible:YES];
+    [_marksmanElement2 setVisible:YES];
+    [_marksmanElement1 setVisible:YES];
+    [_marksmanElement3 setVisible:YES];
+    [_marksmanBranch3 setVisible:YES];
+    [_marksmanBranch2 setVisible:YES];
+    [_marksmanBranch1 setVisible:YES];
+}
+
+- (void) pressedIceSymbol:(id)sender{
+    [_iceMainBranch setVisible:YES];
+    [_iceElement2 setVisible:YES];
+    [_iceElement1 setVisible:YES];
+    [_iceElement3 setVisible:YES];
+    [_iceBranch3 setVisible:YES];
+    [_iceBranch2 setVisible:YES];
+    [_iceBranch1 setVisible:YES];
 }
 
 - (void) pressedMainMenu:(id)sender{
