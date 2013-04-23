7ae0e98634de7149b55ceecaca290860be944bf1
João Machado <joaoprmachado@gmail.com>
2013-04-23 16:10:08 +0100

    New branch

--- TechDemo - Physics/TechDemo - Physics/LevelLayer.m
+++ TechDemo - Physics/TechDemo - Physics/LevelLayer.m
@@ -231,10 +231,15 @@
     location = [[CCDirector sharedDirector] convertToGL:location];
 }
 
--(void)dealloc{
-    [self unscheduleAllSelectors];
-    [[CollisionManager shared] clearAllEntities];
+-(void)onExit
+{
     [[Registry shared] clearRegistry];
+    [[CollisionManager shared] clearAllEntities];
+}
+
+-(void)dealloc
+{
+    //[self unscheduleAllSelectors];
     [super dealloc];
     //CCLOG(@"DEALOQUEI");
 }
